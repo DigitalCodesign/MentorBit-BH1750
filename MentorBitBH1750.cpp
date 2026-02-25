040a028e193534d78b153a6c35a4758af8585563
krosDC <kevin.ingenieria@digitalcodesign.com>
2026-02-25 12:35:26 +0000

    Comments updated.

--- MentorBitBH1750.cpp
+++ MentorBitBH1750.cpp
@@ -16,18 +16,20 @@
         
 
     Autor: Digital Codesign
-    Version: 1.0.0
+    Version: 1.1.0
     Fecha de creación: Enero de 2024
-    Fecha de version: Enero de 2024
+    Fecha de version: Febrero de 2026
     Repositorio: https://github.com/DigitalCodesign/MentorBit-BH1750
     Descripcion: 
         Esta libreria esta especificamente diseñada para ser utilizada junto con 
-        el modulo MentorBit BH1750
-    Metodos principales:
-        MentorBitBH1750 -> constructor de la clase
-        begin -> inicializador, debe colocarse en el setup
-        leerTVOC -> lee el valor de TVOC del sensor
+        el modulo MentorBit BH1750. En esta versión se eliminan dependencias de 
+        terceros (lectura directa por I2C nativo) para mayor eficiencia y menor 
+        consumo de memoria.
 
+    Metodos principales:
+        MentorBit_BH1750(uint8_t i2c_addr) -> Constructor de la clase. Permite cambiar la dirección I2C (0x23 por defecto).
+        begin() -> Inicializa el bus I2C y el sensor en modo continuo. Debe colocarse en el setup(). Devuelve 'true' si lo encuentra.
+        leerLux() -> Devuelve el nivel de iluminación en Lux (float). Devuelve -1.0 si hay un error de conexión.
 */
 
 #include "MentorBitBH1750.h"
